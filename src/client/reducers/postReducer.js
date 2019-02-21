bd431e6e98e1eda75bf98d74470cab022172af95
meriem memmiche <meriemmemmiche@gmail.com>
2019-02-21 12:03:56 +0100

    update

--- src/client/reducers/postReducer.js
+++ src/client/reducers/postReducer.js
@@ -1,7 +1,7 @@
 export default function(state = null, action){
     switch(action.type){
-        case 'FETCH_POST': 
-            return action.payload.data.Blog || false;
+        case 'FETCH_POST':
+            return action.payload.data.blogs || false;
         case 'CLEAR_POST_DATA':
             return null
         default:
